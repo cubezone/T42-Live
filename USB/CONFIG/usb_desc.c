1ca4cd0790e2a4a1439086bd7f94346ebbaa5a9f
cubezone <sisdaemo@hotmail.com>
2017-03-28 15:59:06 +0800

    0x65->0xFF

--- USB/CONFIG/usb_desc.c
+++ USB/CONFIG/usb_desc.c
@@ -173,10 +173,10 @@ const u8 KeyboardReportDescriptor[KP_ReportDescriptor_Size]=
     0x95, 0x06,                    //   REPORT_COUNT (6)
     0x75, 0x08,                    //   REPORT_SIZE (8)
     0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+    0x25, 0xff,                    //   LOGICAL_MAXIMUM (101)
     0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
     0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
+    0x29, 0xff,                    //   USAGE_MAXIMUM (Keyboard Application)
     0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
     0xc0,                          // 	END_COLLECTION
 	//0xc0,
